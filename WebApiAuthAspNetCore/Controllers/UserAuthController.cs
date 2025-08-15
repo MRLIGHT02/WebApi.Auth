adfdf59ca8da85b436aab9dd347612749173d51c
MRLIGHT02 <saifali42020@gmail.com>
2025-08-15 19:28:59 +0530

    Add WebApiAuthAspNetCore project and user registration
    - Updated solution file to include new project.
    - Configured logging in appsettings files.
    - Set up ASP.NET Core application in Program.cs.
    - Targeted .NET 9.0 in project file with necessary packages.
    - Added HTTP file for weather forecast endpoint.
    - Implemented user registration in UserAuthController.
    - Defined User entity and UserDto for registration.
    - Configured launch settings for development environment.

---
+++ WebApiAuthAspNetCore/Controllers/UserAuthController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebApiAuthAspNetCore.Entities;
+using WebApiAuthAspNetCore.Models;
+
+namespace WebApiAuthAspNetCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserAuthController : ControllerBase
+    {
+        private readonly User _user;
+
+        public UserAuthController(User user)
+        {
+            _user = user;
+        }
+
+        [HttpPost]
+        [Route("register")]
+        public ActionResult<User> Register(UserDto request)
+        {
+            var hashedPassword = new PasswordHasher<User>().HashPassword(_user, request.Password);
+            _user.Username = request.Username;
+            _user.PasswordHash = hashedPassword;
+
+            return Ok(_user);
+
+
+        }
+    }
+}
